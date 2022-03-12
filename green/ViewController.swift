a073672466e8138f148d592837503ace9254a63b
Muhammadjon <20mamarasulov@gmail.com>
2022-03-12 17:21:45 +0900

    Initial Commit

---
+++ green/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  green
+//
+//  Created by Muhammadjon Mamarasulov on 3/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
